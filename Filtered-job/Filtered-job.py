7b4f4e85d9334d6de981f874672d2d2ae99d0640
Paolo Silver Portuguez Hilares <silverpaolo09@gmail.com>
2026-04-19 19:51:18 -0500

    Paso: adicionar lógica de cambio de nombre de parquet

--- Filtered-job/Filtered-job.py
+++ Filtered-job/Filtered-job.py
@@ -7,9 +7,13 @@ from awsglue.dynamicframe import DynamicFrame
 from awsglue.job import Job
 from pyspark.sql.functions import *
 
+import boto3
+import time
+
 
 ## @params: [JOB_NAME]
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+#args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+args = getResolvedOptions(sys.argv, ['JOB_NAME','JOB_RUN_ID'])
 
 # Ruta S3:
 s3_path = f"s3://bucket-bootcamp-bronze-0001/scripts-py/transactions/cards_transactions.csv"
@@ -53,13 +57,69 @@ transactions_df.show()
 #Convertir DF a DYF                   
 final_dyf = DynamicFrame.fromDF(transactions_df,glueContext,"final_dyf")
 
+
+bucket = "bucket-bootcamp-silver-0001"
+run_id = args['JOB_RUN_ID']
+# Ruta temporal única por ejecución
+temp_prefix = f"tmp/transactions/{run_id}/"
+# Nombre final del parquet
+final_key = "scripts-py/transactions-outputs/transactions_final.parquet"
+
+# Convertimos a DF y forzamos un solo archivo
+# ==========================================
+final_df = final_dyf.toDF().coalesce(1)
+
+# Ahora usamos DataFrame.write porque permite:
+# - uncompressed
+# - control para renombrar después
+# ==========================================
+final_df.write \
+    .mode("overwrite") \
+    .option("compression", "uncompressed") \
+    .parquet(
+        f"s3://{bucket}/{temp_prefix}"
+    )
+
+# Renombrar el part-xxxxx.parquet
+# a transactions_final.parquet
+# ==========================================
+s3 = boto3.client("s3")
+
+response = s3.list_objects_v2(
+    Bucket=bucket,
+    Prefix=temp_prefix
+)
+
+for obj in response.get("Contents", []):
+    key = obj["Key"]
+    if key.endswith(".parquet"):
+        s3.copy_object(
+            Bucket=bucket,
+            CopySource={
+                "Bucket": bucket,
+                "Key": key
+            },
+            Key=final_key
+        )
+        print(
+            f"Archivo final generado: s3://{bucket}/{final_key}"
+        )
+        break
+
+for obj in response.get("Contents", []):
+
+    s3.delete_object(
+        Bucket=bucket,
+        Key=obj["Key"]
+    )
+
 #Generar S3 parquet.
-write_final_dyf =glueContext.write_dynamic_frame.from_options(
-                    frame = final_dyf,
-                    connection_type = "S3", 
-                    connection_options={"path":"s3://bucket-bootcamp-silver-0001/scripts-py/transactions-outputs/"}, 
-                    format="parquet", 
-                    transformation_ctx="write_final_dyf"
-                )
+#write_final_dyf =glueContext.write_dynamic_frame.from_options(
+#                    frame = final_dyf,
+#                    connection_type = "S3", 
+#                    connection_options={"path":"s3://bucket-bootcamp-silver-0001/scripts-py/transactions-outputs/"}, 
+#                    format="parquet", 
+#                    transformation_ctx="write_final_dyf"
+#                )
 
 job.commit()
